b5e2a0df8a83c7bb37162a9333b14e4b7b0c05e0
Vinicius <vinny_aguiar@hotmail.com>
2025-03-14 10:43:10 -0300

    :sparkles: feat: Implementando a requisição POST para envio de dados

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
 import sqlite3
 
 app = Flask(__name__)
 
 # route --> São os endpoint da nossa API
-@app.route("/")
-def pagar_pessoas():
-    return "<h1>Começar a semana, pagando suas dívidas, é bom demais</h1>"
-
-@app.route("/pix")
-def mande_o_pix():
-    return "<h3>Pagar as pessoas faz bem pra pessoas!! =D</h3>"
 
 def init_db():
     # Crie o nosso banco de dados com um arquivo'database.db' e conecte a variável conn (connection)
@@ -28,6 +21,21 @@ def init_db():
             """
         )
 init_db()
+
+app.route("/doar", methods=["POST"])
+def doar():
+    # Variável dados receba a resposta do cliente em JSON
+    dados = request.get_json()
+
+    titulo = dados.get("titulo")
+    categoria = dados.get("categoria")
+    autor = dados.get("autor")
+    imagem_url = dados.get("imagem_url")
+
+    if not titulo or not categoria or not autor or not imagem_url:
+        return jsonify({"erro":"Todos os campos são obrigatórios"}),400 # 400 Indica que o servidor não pode processar uma solicitação
+
+
 
 
 
