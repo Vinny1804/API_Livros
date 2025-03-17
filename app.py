63fc1cdfcdfee03d37ae609567ab01c067df6d72
Vinicius <vinny_aguiar@hotmail.com>
2025-03-17 10:58:21 -0300

    :sparkles: feat: Enviando arquivos para o banco de dados

--- app.py
+++ app.py
@@ -16,13 +16,14 @@ def init_db():
                     titulo TEXT NOT NULL,
                     categoria TEXT NOT NULL,
                     autor TEXT NOT NULL,
-                    imagem_url TEXT NOT NULL
+                    image_url TEXT NOT NULL
                 )
             """
         )
 init_db()
 
-app.route("/doar", methods=["POST"])
+
+@app.route("/doar", methods=["POST"])
 def doar():
     # Variável dados receba a resposta do cliente em JSON
     dados = request.get_json()
@@ -30,18 +31,22 @@ def doar():
     titulo = dados.get("titulo")
     categoria = dados.get("categoria")
     autor = dados.get("autor")
-    imagem_url = dados.get("imagem_url")
+    image_url = dados.get("image_url")
 
-    if not titulo or not categoria or not autor or not imagem_url:
+    if not titulo or not categoria or not autor or not image_url:
         return jsonify({"erro":"Todos os campos são obrigatórios"}),400 # 400 Indica que o servidor não pode processar uma solicitação
 
+    with sqlite3.connect("database.db") as conn:
 
+        conn.execute(f"""
+        INSERT INTO LIVROS (titulo, categoria, autor, image_url)
+        VALUES ("{titulo}", "{categoria}", "{autor}", "{image_url}")
+        """)
 
+    conn.commit()
 
+    return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
 
 # Se o arquivo app.py == ao arquivo principal da nossa aplicação
 if __name__ == "__main__":
     app.run(debug=True)
-
-# # É o comando para rodar a nossa aplicação
-# app.run()
